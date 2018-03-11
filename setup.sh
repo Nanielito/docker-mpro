0ff19ac9f78cc1c8f56617bb8bb4e21975ec333a
nanielito <danielramirezvi@gmail.com>
2018-03-10 22:53:12 -0400

    #M Setup script was updated to clone repository and run Dockerfile

--- setup.sh
+++ setup.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-OPTS=`getopt -o hr:b:u:p:t: --long help,repository:,branch:,registry-user:,registry-password:,tag: -n 'parse-options' -- "$@"`
+OPTS=`getopt -o hr:b:t: --long help,repository:,branch:,tag: -n 'parse-options' -- "$@"`
 eval set -- "$OPTS"
 
 function usage() {
-  printf "Usage: $0 -r|--repository REPOSITORY [-b|--branch BRANCH] -u|--regisrty-user USER -p|--registry-password PASSWORD [-t|--tag TAG]"
+  printf "Usage: $0 -r|--repository REPOSITORY [-b|--branch BRANCH] [-t|--tag TAG]"
 }
 
 REPOSITORY=""
 BRANCH=""
-REGISTRY_USER=""
-REGISTRY_PASSWORD=""
 TAG=""
 
 while true
@@ -22,10 +20,6 @@ while true
         REPOSITORY=$2; shift 2 ;;
       -b | --branch)
         BRANCH=$2; shift 2 ;;
-      -u | --registry-user)
-        REGISTRY_USER=$2; shift 2 ;;
-      -p | --registry-password)
-        REGISTRY_PASSWORD=$2; shift 2 ;;
       -t | --tag)
         TAG=$2; shift 2 ;;
       --)
@@ -35,7 +29,7 @@ while true
     esac
   done
 
-if [ -z "$REPOSITORY" ] || [ -z "$REGISTRY_USER" ] || [ -z "$REGISTRY_PASSWORD" ]; then
+if [ -z "$REPOSITORY" ]; then
   usage
   exit 0
 fi
@@ -52,11 +46,5 @@ echo "Cloning $REPOSITORY on branch $BRANCH..."
 git clone -b $BRANCH $REPOSITORY ./mpro
 
 docker build -t mpro/mpro-app:$TAG .
-
-docker login -u $REGISTRY_USER -p $REGISTRY_PASSWORD
-
-docker tag mpro/mpro-app $REGISTRY_USER/mpro-app
-
-docker push $REGISTRY_USER/mpro-app
 
 rm -rf mpro
