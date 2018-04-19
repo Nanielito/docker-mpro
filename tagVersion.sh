5394eeacd1a7c76e6f7f45137d21c61e194a1add
nanielito <danielramirezvi@gmail.com>
2018-04-19 00:39:54 -0400

    #M Minor adjustment on tagVersion script to push a tag into repository

--- tagVersion.sh
+++ tagVersion.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+OPTS=`getopt -o hr: --long help,repository: -n 'parse-options' -- "$@"`
+eval set -- "$OPTS"
+
+function usage() {
+  printf "Usage: $0 -r|--repository REPOSITORY"
+}
+
 function setupGit() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
@@ -7,14 +14,36 @@ function setupGit() {
 
 function tagVersion() {
   TAG=$1
+  REPOSITORY=$(echo $2 | sed -e s#github#\$\{GH_TOKEN\}@github#g)
 
   git tag $TAG -a -m "Release version $TAG"
+  git remote add origin $REPOSITORY > /dev/null 2>&1
   git push origin $TAG
 }
 
 function main() {
+  REPOSITORY=""
   BRANCH=""
   TAG=""
+
+  while true
+    do
+      case "$1" in
+        -h | --help)
+          usage; exit 0 ;;
+        -r | --repository)
+          REPOSITORY=$2; shift 2 ;;
+        --)
+          shift; break ;;
+        *)
+          usage; exit 1 ;;
+      esac
+    done
+
+  if [ -z "$REPOSITORY" ]; then
+    usage
+    exit 1
+  fi
 
   cd mpro
 
@@ -24,7 +53,7 @@ function main() {
     TAG=$(bash scripts/appVersion.sh --version)
 
     setupGit
-    tagVersion $TAG
+    tagVersion $TAG $REPOSITORY
   fi
 
   cd ..
