f4f80fef70c4d32c56a23fa62ee7ddaf8db99da6
nanielito <danielramirezvi@gmail.com>
2018-04-22 15:44:52 -0400

    #N updatePhase.sh script was added to update package version to next development phase

---
+++ scripts/updatePhase.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+OPTS=`getopt -o hr: --long help,repository: -n 'parse-options' -- "$@"`
+eval set -- "$OPTS"
+
+function usage() {
+  printf "Usage: $0 -r|--repository REPOSITORY"
+}
+
+function updateDevelopmentVersion() {
+  VERSION=""
+  MASTER=$1
+  DEVELOPMENT=$2
+  REPOSITORY=$(echo $3 | sed -e s#github#${GH_USER}\:${GH_TOKEN}@github#g)
+  USER=$(git config user.name)
+
+  git checkout $DEVELOPMENT
+  git merge $MASTER
+
+  $(bash scripts/appVersion.sh --next)
+
+  git add package.json 
+  git commit -m "$USER: Package version was updated to next development phase"
+  git push --quiet $REPOSITORY $DEVELOPMENT > /dev/null 2>&1
+}
+
+REPOSITORY=""
+BRANCH=""
+
+while true
+  do
+    case "$1" in
+      -h | --help)
+        usage; exit 0 ;;
+      -r | --repository)
+        REPOSITORY=$2; shift 2 ;;
+      --)
+        shift; break ;;
+      *)
+        usage; exit 1 ;;
+    esac
+  done
+
+if [ -z "$REPOSITORY" ]; then
+  usage
+  exit 1
+fi
+
+cd mpro
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$BRANCH" = "${RELEASE_BRANCH}" ]; then
+  updateDevelopmentVersion ${RELEASE_BRANCH} ${DEVELOPMENT_BRANCH} $REPOSITORY
+else
+  echo "Version still in development phase"
+fi
+
+cd ..
+
+rm -rf mpro $VERSION.tgz
